6d0dea9b4460d2448189a8f9b83dbdc8c5de0bb9
IT24100546 <it24100546@my.sliit.lk>
2024-09-20 12:19:31 +0530

    Lab7Q2A

---
+++ Q2/IT24100546Lab7Q2A.java
@@ -0,0 +1,23 @@
+public class IT24100546Lab7Q2A{
+	public static void main(String[] args){
+
+	int rows=4;
+	int columns = 5;
+
+	for(int i=1; i<=rows; i++){
+		
+		for(int j=1; j<=columns; j++){
+			System.out.print("$ ");
+		}
+
+
+	System.out.println();
+
+	}
+
+
+}
+
+
+}
+
